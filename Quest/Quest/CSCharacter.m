71ec9118471f34ad0ea155d32440c89953118e1a
coody <coody0829@gmail.com>
2016-01-24 19:16:13 +0800

    1. 加入 debug 的綠色框框線 2. plist 加入更多屬性

--- Quest/Quest/CSCharacter.m
+++ Quest/Quest/CSCharacter.m
@@ -11,12 +11,22 @@
 // for constant
 #import "Constants.h"
 
+typedef enum{
+    EnumZPosition_CSCharacter_Shape = 1000,
+}EnumZPosition_CSCharacter;
+
 @interface CSCharacter()
 {
     // actual image
     SKSpriteNode *character;
     
     NSDictionary *characterData;
+    
+    BOOL useForCollisions;
+    
+    float collisionBodyCoversWhatPercent;
+    
+    unsigned char collisionBodyType; // 0~255
     
 }
 @end
@@ -35,12 +45,53 @@
 
 -(void)createWithDictionary:(NSDictionary *)charData{
     
+    
     characterData = [NSDictionary dictionaryWithDictionary:charData];
     
     character = [SKSpriteNode spriteNodeWithImageNamed:[characterData objectForKey:@"BaseFrame"]];
     [self addChild:character];
     
-    self.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:character.frame.size.width/2];
+    useForCollisions = [[characterData objectForKey:@"UseForCollisions"] boolValue];
+    
+    if ( useForCollisions == YES ) {
+        [self setupPhysics];
+    }
+}
+
+-(void)setupPhysics{
+    
+    
+    collisionBodyCoversWhatPercent = [[characterData objectForKey:@"CollisionBodyCoversWhatPercent"] floatValue];
+    CGSize newSize = CGSizeMake(character.size.width * collisionBodyCoversWhatPercent,
+                                character.size.height * collisionBodyCoversWhatPercent);
+    
+    /////////////// 重點：設定兩種不同形狀的碰撞 body，方形、或圓形
+    if ( [[characterData objectForKey:@"CollisionBodyType"] isEqualToString:@"square"] ) {
+        collisionBodyType = squareType;
+        self.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:newSize];
+        if ( [[characterData objectForKey:@"DebugBody"] boolValue] == YES ) {
+            CGRect rect = CGRectMake(-(newSize.width/2),
+                                     -(newSize.height/2),
+                                     newSize.width,
+                                     newSize.height);
+            [self debugPath:rect bodyType:collisionBodyType];
+        }
+    }
+    else{
+        collisionBodyType = circleType;
+        self.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:newSize.width/2];
+        if ( [[characterData objectForKey:@"DebugBody"] boolValue] == YES ) {
+            CGRect rect = CGRectMake(-(newSize.width/2),
+                                     -(newSize.height/2),
+                                     newSize.width,
+                                     newSize.height);
+            [self debugPath:rect bodyType:collisionBodyType];
+        }
+    }
+    
+    
+    
+    /////////////// 重要：設定 physics body /////////////////
     self.physicsBody.dynamic = YES;
     self.physicsBody.restitution = 1.5;
     self.physicsBody.allowsRotation = YES;
@@ -48,6 +99,28 @@
     self.physicsBody.categoryBitMask = playerCategory;
     self.physicsBody.collisionBitMask = wallCategory;   // collision 碰撞的意思
     self.physicsBody.contactTestBitMask = wallCategory; // 碰撞後會聯繫的 category , 可以用 | 來聯繫多個 body
+}
+
+-(void)debugPath:(CGRect)theRect bodyType:(int)type{
+    
+    SKShapeNode *pathShape = [[SKShapeNode alloc] init];
+    CGPathRef thePath ;
+    if ( type == squareType ) {
+         thePath = CGPathCreateWithRect( theRect , NULL);
+    }
+    else{
+        thePath = CGPathCreateWithEllipseInRect( theRect , NULL);
+    }
+    
+   
+    pathShape.path = thePath;
+    
+    pathShape.lineWidth = 1;
+    pathShape.strokeColor = [SKColor greenColor];
+    pathShape.position = CGPointMake(0, 0);
+    
+    pathShape.zPosition = EnumZPosition_CSCharacter_Shape;
+    [self addChild:pathShape];
     
 }
 
--- Quest/Quest/CSLevel.m
+++ Quest/Quest/CSLevel.m
@@ -14,6 +14,12 @@
 // for node
 #import "CSCharacter.h"
 
+typedef enum{
+    EnumZPosition_Level_Map = 10,
+    EnumZPosition_Level_Leader = 20,
+    EnumZPosition_Level_Shape = 1000,
+}EnumZPosition_Level;
+
 @interface CSLevel() < SKPhysicsContactDelegate >
 {
     int currentLevel;
@@ -42,6 +48,7 @@
 
 -(void)setUpScene{
     
+    ////////////////////// 重點：從 plist 取得關卡資料、並且載入 ////////////////////
     // 取得 level 以及 character 的資料，從 .plist 中
     NSString *path = [[NSBundle mainBundle] bundlePath];
     NSString *finalPath = [path stringByAppendingPathComponent:@"GameData.plist"];
@@ -49,30 +56,70 @@
     
     NSLog(@" plist = %@" , plistData);
     
+    // 取得 level 資料
     NSMutableArray *levelArray = [NSMutableArray arrayWithArray:[plistData objectForKey:@"Levels"]];
     NSDictionary *levelDict = [NSDictionary dictionaryWithDictionary:[levelArray objectAtIndex:currentLevel]];
     characterArray = [NSArray arrayWithArray:[levelDict objectForKey:@"Characters"]];
     NSLog(@" character array = %@" , characterArray);
     
-    //
+    // 初始化 myWord 的基礎 SKNode
     self.anchorPoint = CGPointMake(0.5, 0.5);
     myWorld = [SKNode node];
     [self addChild:myWorld];
     
+    // 設定背景樣式（使用 SKSpriteNode ）
     SKSpriteNode *map = [SKSpriteNode spriteNodeWithImageNamed:[levelDict objectForKey:@"Background"]];
     map.position = CGPointMake(0, 0);
     [myWorld addChild:map];
-    map.zPosition = 10;
+    map.zPosition = EnumZPosition_Level_Map;
     
-    // physics
+    // 設定真正 physics 碰撞的 frame
+    float shrinkage = [[levelDict objectForKey:@"ShrinkBackgroundBoundaryBy"] floatValue];
+    // 計算出篇移量的 1/2 ，讓碰撞的綠色框框會在整張大圖的正中央
+    // (因為碰撞的綠色框框比較小 0.9 倍的 map.frame 而已)
+    int offsetX = (map.frame.size.width - (map.frame.size.width * shrinkage)) / 2;
+    int offsetY = (map.frame.size.height - (map.frame.size.height * shrinkage)) / 2;
+    CGRect mapWithSmallerRect = CGRectMake(map.frame.origin.x + offsetX,
+                                           map.frame.origin.y + offsetY,
+                                           map.frame.size.width * shrinkage,
+                                           map.frame.size.height * shrinkage);
+    
+    
+    //////////////////// 重點：設定 physics world ///////////////////
+    // 設定 physicsWorld 的重力（x,y）
     self.physicsWorld.gravity = CGVectorMake(0.5, -0.9);
+    // 設定
     self.physicsWorld.contactDelegate = self;
     
-    myWorld.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:map.frame];
-    myWorld.physicsBody.categoryBitMask = wallCategory;
+    /////////////////// 重點：設定玩 physics world 後，再設定 physics body /////////////////
+    myWorld.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:mapWithSmallerRect];
+    myWorld.physicsBody.categoryBitMask = wallCategory;// 給這個碰撞的 body 一個 tag
+    
+    if ( [[levelDict objectForKey:@"DebugBroader"] boolValue] == YES ) {
+        [self debugPath:map.frame];
+    }
     
 }
 
+
+///////////////// 重點：利用 SKShapeNode 繪製出碰撞的範圍 ////////////
+-(void)debugPath:(CGRect)theRect{
+    
+    SKShapeNode *pathShape = [[SKShapeNode alloc] init];
+    CGPathRef thePath = CGPathCreateWithRect( theRect , NULL);
+    pathShape.path = thePath;
+    
+    pathShape.lineWidth = 2;
+    pathShape.strokeColor = [SKColor greenColor];
+    pathShape.position = CGPointMake(0, 0);
+    
+    
+    pathShape.zPosition = EnumZPosition_Level_Shape;
+    [myWorld addChild:pathShape];
+    
+}
+
+//////////////// 重點：實作 SKPhysicsContactDelegate 的碰撞回呼 /////////////
 -(void)didBeginContact:(SKPhysicsContact *)contact{
     SKPhysicsBody *firstBody , *secondBody;
     
@@ -95,7 +142,7 @@
     [leader createWithDictionary:[characterArray objectAtIndex:0]];
     
     [myWorld addChild:leader];
-    leader.zPosition = 20;
+    leader.zPosition = EnumZPosition_Level_Leader;
     
 }
 
